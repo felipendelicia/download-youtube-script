7d137d7c06d56797fb2085aa3e2aec5b4f6a695e
felipendelicia <delicia4581@gmail.com>
2023-10-31 19:36:06 -0300

    fixed not alpha chars

--- index.py
+++ index.py
@@ -1,5 +1,13 @@
 from pytube import Playlist
 
+def getSongFileName(videoTitle:str):
+    song_name = "";
+    for letter in videoTitle:
+        if letter.isspace() or letter.isalnum():
+            song_name += letter
+    return song_name + ".mp3"
+
+
 print("Download youtube script por felipo.")
 print("Repository: https://github.com/felipendelicia")
 print()
@@ -12,7 +20,10 @@ print("Downloading songs...")
 
 for video in youtubePlaylist.videos:
     print("", "Starting download of:", video.title)
-    song_name = video.title + ".mp3"
-    video.streams.get_audio_only().download(filename=song_name)
+    song_name = getSongFileName(video.title)
+    try:
+        video.streams.get_audio_only().download("./songs",filename=song_name)
+    except Exception as e:
+        print(f"An error occurred while downloading {song_name}: {e}")
 
 input("Press any key to exit.")
