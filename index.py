6fdf6b0fd1e2525c1ad60eb8101f12356ff84559
felipe <delicia4581@gmail.com>
2023-11-10 14:26:41 -0300

    added download only song option

--- index.py
+++ index.py
@@ -1,29 +1,67 @@
-from pytube import Playlist
+from pytube import Playlist, YouTube
 
-def getSongFileName(videoTitle:str):
-    song_name = "";
+
+def getSongFileName(videoTitle: str):
+    song_name = ""
     for letter in videoTitle:
         if letter.isspace() or letter.isalnum():
             song_name += letter
     return song_name + ".mp3"
 
 
+def downloadSong(song):
+    name = getSongFileName(song.title)
+    song.streams.get_audio_only().download("./songs", filename=name)
+
+
+def downloadOneSong():
+    youtubeSongUrl = input("Enter youtube song URL: ")
+    youtubeSong = YouTube(youtubeSongUrl)
+
+    try:
+        downloadSong(youtubeSong)
+    except Exception as e:
+        print(f"An error occurred while downloading", youtubeSong.title)
+
+
+def downloadOnePlaylist():
+    youtubePlaylistUrl = input("Enter the youtube playlist URL: ")
+
+    youtubePlaylist = Playlist(youtubePlaylistUrl)
+
+    print("Downloading songs...")
+
+    for video in youtubePlaylist.videos:
+        print("", "Starting download of:", video.title)
+        try:
+            downloadSong(video)
+        except Exception as e:
+            print(f"An error occurred while downloading", video.title)
+
+
+def checkResponse(response: str):
+    response = response.lower()
+    return response == "a" or response == "b"
+
+
 print("Download youtube script por felipo.")
 print("Repository: https://github.com/felipendelicia")
 print()
 
-youtubePlaylistUrl = input("Enter the youtube playlist URL: ")
+print("You want download:")
+print("A - single song")
+print("B - complete playlist")
 
-youtubePlaylist = Playlist(youtubePlaylistUrl)
+correctAnswer = False
+respose = ""
 
-print("Downloading songs...")
+while not correctAnswer:
+    response = input("Enter your choise (A/B): ")
+    correctAnswer = checkResponse(response)
 
-for video in youtubePlaylist.videos:
-    print("", "Starting download of:", video.title)
-    song_name = getSongFileName(video.title)
-    try:
-        video.streams.get_audio_only().download("./songs",filename=song_name)
-    except Exception as e:
-        print(f"An error occurred while downloading {song_name}: {e}")
+if response == "a":
+    downloadOneSong()
+else:
+    downloadOnePlaylist()
 
 input("Press any key to exit.")
